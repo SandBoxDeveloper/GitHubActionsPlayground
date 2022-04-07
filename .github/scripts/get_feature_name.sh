920263a274ae2d4bba87a66349540b54d0c5be5d
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2022-04-07 17:35:33 +0100

    - testing script to grab branch name

--- .github/scripts/get_feature_name.sh
+++ .github/scripts/get_feature_name.sh
@@ -11,7 +11,7 @@ do
     esac
 done
 
-if [ "$FEATURE_BRANCH_NAME" == "ref/head/main" ]; then
+if [ "$FEATURE_BRANCH_NAME" == "refs/heads/main" ]; then
   echo "${FEATURE_BRANCH_NAME##*/}"
 else
   branch_ref="${FEATURE_BRANCH_NAME%/*}"
