ac333b5b98988a73f268d096c305626a463a3fc9
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2022-04-08 19:33:38 +0100

    - testing distributing to different app center folders

--- .github/scripts/distribution_folder_name.sh
+++ .github/scripts/distribution_folder_name.sh
@@ -12,8 +12,10 @@ do
 done
 
 if [ "$FEATURE_BRANCH_NAME" == "refs/heads/main" ]; then
+  echo "feature branch matches main"
   echo "${FEATURE_BRANCH_NAME##*/}"
 else
-  branch_ref="${FEATURE_BRANCH_NAME%/*}"
+  branch_ref="${FEATURE_BRANCH_NAME{0##*/}}"
+  echo "branch does not match main"
   echo "${branch_ref##*/}"
 fi
