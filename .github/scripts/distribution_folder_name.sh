57cfa897be4443be9f6c4323b916e1a142cd580b
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2022-04-08 19:38:47 +0100

    - testing distributing to different app center folders

--- .github/scripts/distribution_folder_name.sh
+++ .github/scripts/distribution_folder_name.sh
@@ -11,11 +11,12 @@ do
     esac
 done
 
-if [ "$FEATURE_BRANCH_NAME" == "refs/heads/main" ]; then
+if [ "$FEATURE_BRANCH_NAME" == "refs/heads/main" ]
+then
   echo "feature branch matches main"
   echo "${FEATURE_BRANCH_NAME##*/}"
 else
-  branch_ref="${FEATURE_BRANCH_NAME{0##*/}}"
+  branch_ref="${FEATURE_BRANCH_NAME%/*}"
   echo "branch does not match main"
   echo "${branch_ref##*/}"
 fi
