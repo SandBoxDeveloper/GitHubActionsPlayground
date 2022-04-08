40776859a60307e2ebf0f6473894a09f1e7600d6
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2022-04-08 15:29:30 +0100

    - testing script to grab branch name

--- .github/scripts/app_center_release_notes.sh
+++ .github/scripts/app_center_release_notes.sh
@@ -14,7 +14,7 @@ getLastCommitDateTime() {
 
 releaseNotes() {
   printf "Git hash: %s" "$(getGitSha)"
-  echo -e " \n\nLast changes: \n%s " "$(getLastCommitMessages)"
+  printf " \n\nLast changes: \n%s " "$(getLastCommitMessages)"
   printf "\n\nLast change date:\n%s" "$(getLastCommitDateTime)"
 }
 
