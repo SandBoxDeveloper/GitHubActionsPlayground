79f764a8ed707eb7178acb2a1e8fabd079f1dda1
andrehitchman <madetobethebest@hotmail.co.uk>
2023-10-02 20:40:47 +0100

    - updated cmd line tools download link to a later version

--- .github/scripts/install_android.sh
+++ .github/scripts/install_android.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Define Android SDK tools URL and directory
-SDK_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
+SDK_URL="https://dl.google.com/android/repository/commandlinetools-mac-10406996_latest.zip"
 SDK_DIR="$HOME/android-sdk"
 
 # Print the value of SDK_DIR
@@ -20,7 +20,7 @@ rm -f sdk.zip
 
 # Set environment variables
 export ANDROID_SDK_ROOT="$SDK_DIR"
-export PATH="$PATH:$ANDROID_SDK_ROOT/cmdline-tools/bin"
+export PATH="$PATH:$ANDROID_SDK_ROOT/tools/bin"
 
 # Print the value of SDK_DIR
 echo "ANDROID_SDK_ROOT is set to: $ANDROID_SDK_ROOT"
