a4b6c9390cf99233a33b3365289e923e495a333b
andrehitchman <madetobethebest@hotmail.co.uk>
2023-10-03 00:00:50 +0100

    - debugging

--- .github/scripts/start_emulator.sh
+++ .github/scripts/start_emulator.sh
@@ -8,11 +8,12 @@ AVD_OPTIONS="-no-snapshot -no-audio -no-window"
 # Create and start the emulator
 echo "Creating and starting the Android emulator..."
 echo "no" | $ANDROID_HOME/tools/bin/avdmanager create avd -n "$AVD_NAME" -k "system-images;android-30;google_apis;x86_64"
-emulator @$AVD_NAME $AVD_OPTIONS &
+$ANDROID_SDK_ROOT/tools/emulator @$AVD_NAME $AVD_OPTIONS &
 
 # Wait for the emulator to fully start
 echo "Waiting for the emulator to start..."
-$ANDROID_SDK_ROOT/emulator/emulator -avd $AVD_NAME -no-boot-anim -no-window -gpu swiftshader -no-snapshot -camera-back none -camera-front none -selinux disabled -qemu -m 2048 &
+#$ANDROID_SDK_ROOT/tools/emulator/emulator -avd $AVD_NAME -no-boot-anim -no-window -gpu swiftshader -no-snapshot -camera-back none -camera-front none -selinux disabled -qemu -m 2048 &
+$ANDROID_SDK_ROOT/tools/emulator -avd $AVD_NAME -no-boot-anim -no-window -gpu swiftshader -no-snapshot -camera-back none -camera-front none -selinux disabled -qemu -m 2048 &
 
 # Wait for the emulator to fully start (adjust the timeout as needed)
-timeout 10m $ANDROID_SDK_ROOT/tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
+timeout 10m $ANDROID_SDK_ROOT/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
