525dd383949209a041bdb65ea835bac25278190d
andrehitchman <madetobethebest@hotmail.co.uk>
2023-10-03 20:09:48 +0100

    - debugging : update list devices script

--- .github/scripts/list_devices.sh
+++ .github/scripts/list_devices.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-function list_devices() {
-  # Check for connected devices and emulators
-  devices=$(adb devices | awk 'NR>1 {print $1}')
+# Check for connected devices and emulators
+devices=$(adb devices | awk 'NR>1 {print $1}')
 
-  # Check if there are devices
-  if [ -n "$devices" ]; then
-    echo "There are devices"
+# Check if there are devices
+if [ -n "$devices" ]; then
+  echo "There are devices"
 
-    # Loop through the list and kill each device
-    for device in $devices; do
-      adb -s "$device" emu kill 2>/dev/null || adb -s "$device" kill-server
-    done
-  else
-    echo "No devices found"
-  fi
-}
-
-list_devices
+  # Loop through the list and kill each device
+  for device in $devices; do
+    adb -s "$device" emu kill 2>/dev/null || adb -s "$device" kill-server
+  done
+else
+  echo "No devices found"
+fi
